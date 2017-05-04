56522c17f0ddc507e18eb08c9e67d101c22c3999
Demidenko98 <dem-fil@yandex.ru>
2017-05-04 23:12:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double a = 100.2, b = 53.2;
+            //double result = a / b;
+            //System.Console.WriteLine("a=" + a);
+            //System.Console.WriteLine("b=" + b);
+            //System.Console.WriteLine("a/b ={0}", Math.Round(result,3), +result);
+            //System.Console.ReadLine();
+
+            double c=2.5 , d=3.0;
+            //double c = Console.Read();
+            //double d = Console.Read();
+            double S = 0.5 * c * d;
+            System.Console.WriteLine("Площадь прямоугольного треугольника равна: " + S);
+
+        }
+    }
+}
